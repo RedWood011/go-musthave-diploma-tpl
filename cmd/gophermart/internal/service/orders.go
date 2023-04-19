6d87fa865999985ce4234bd38b5a1976df90b4d3
Evgen Yaroshenko <yaev96@list.ru>
2023-04-20 02:23:33 +0300

    fix

--- cmd/gophermart/internal/service/orders.go
+++ cmd/gophermart/internal/service/orders.go
@@ -14,6 +14,7 @@ import (
 const (
 	register = "REGISTERED"
 	process  = "PROCESSING"
+	finish   = "PROCESSED"
 )
 
 type responseOrder struct {
@@ -95,7 +96,7 @@ loop:
 		order.Accrual = res.Accrual
 		order.Status = res.Status
 		saveOrders = append(saveOrders, order)
-		if res.Status == process {
+		if res.Status == finish {
 			usersBalanse = append(usersBalanse, entity.User{
 				ID: order.UserID,
 				UserBalance: entity.UserBalance{
