b66fed5d5cf3d432cbf1c40d940ed3a44ca2b622
Evgen Yaroshenko <yaev96@list.ru>
2023-04-20 22:04:40 +0300

    fix

--- cmd/gophermart/internal/service/orders.go
+++ cmd/gophermart/internal/service/orders.go
@@ -61,7 +61,6 @@ loop:
 		for i := 0; i <= s.cfg.CountRepetitionClient; i++ {
 			url := fmt.Sprintf("%s/api/orders/%s", s.cfg.AccrualSystemAddress, order.Number)
 			response, err = http.Get(url)
-			response.Body.Close()
 			if err != nil || response.StatusCode != http.StatusOK {
 				switch {
 				case i == s.cfg.CountRepetitionClient:
@@ -78,6 +77,7 @@ loop:
 			}
 		}
 		body, err := io.ReadAll(response.Body)
+		defer response.Body.Close()
 		if err != nil {
 			s.logger.Info(fmt.Sprintf("Error read body response: UserID = %s, order number =%s", order.UserID, order.Number))
 			continue
